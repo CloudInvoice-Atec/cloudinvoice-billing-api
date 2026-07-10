5fbe0ee1e3b11175bb335c26abfbff280940a935
guiverdadebuss <guilhermeverdadebuss@gmail.com>
2026-07-10 22:33:06 +0100

    feat: Entities and Interfaces Domain

---
+++ CloudInvoice.Billing/CloudInvoice.Billing.Domain/Entities/Company.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudInvoice.Billing.Domain.Entities
+{
+    public class Company
+    {
+        public int Id { get; set; }
+        public string TradeName { get; set; } = string.Empty;
+        public string LegalName { get; set; } = string.Empty;
+        public string TaxNumber { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+
+    }
+}
---
+++ CloudInvoice.Billing/CloudInvoice.Billing.Domain/Entities/InvoiceLine.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudInvoice.Billing.Domain.Entities
+{
+    public class InvoiceLine
+    {
+        public Guid Id { get; set; }
+        public Guid InvoiceId { get; set; } // Foreign Key
+        public Guid ProductId { get; set; } // Reference to Catalog.API Product
+        public string Description { get; set; } = string.Empty;
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal TaxRate { get; set; } // e.g., 23 for 23%
+
+        // Calculated properties
+        public decimal TaxAmount => UnitPrice * (TaxRate / 100) * Quantity;
+        public decimal LineTotal => (UnitPrice * Quantity) + TaxAmount;
+
+        // Navigation Property
+        public Invoice Invoice { get; set; } = null!;
+    }
+}
